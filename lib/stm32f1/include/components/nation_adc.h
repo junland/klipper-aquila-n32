c381aa258cfd30c1ea65b9414fc75a9c655d77e5
junland <unlandj2012@gamil.com>
2022-11-10 13:08:48 -0600

    Change nation_adc

--- lib/stm32f1/include/components/nation_adc.h
+++ lib/stm32f1/include/components/nation_adc.h
@@ -1,9 +1,6 @@
 /**
  *
- * Copyright (c) 2020 Marlin Firmware [https://github.com/MarlinFirmware/Marlin]
- * Copyright (c) 2016 Bob Cousins bobcousins42@googlemail.com
- * Copyright (c) 2015-2016 Nico Tonnhofer wurstnase.reprap@gmail.com
- * Copyright (c) 2017 Victor Perez
+ * Copyright (c) 2022 Alexey (stas2z)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,6 +14,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0
  *
  */
 
